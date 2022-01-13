d5b5ea014227b260383f82f8a77ea1a3a6f0f735
David Chan <dchan902@gmail.com>
2022-01-12 19:05:51 -0700

    modify 1_queries

--- 1_queries/1_all_my_reservations.sql
+++ 1_queries/1_all_my_reservations.sql
@@ -1,9 +1,8 @@
-SELECT properties.*, reservations.*, avg(property_reviews.rating) as average_rating
-FROM properties
-JOIN reservations ON properties.id = reservations.property_id
+SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
 JOIN property_reviews ON properties.id = property_reviews.property_id
-WHERE property_reviews.guest_id = 1
-AND end_date < NOW()
+WHERE reservations.guest_id = 1
 GROUP BY properties.id, reservations.id
-ORDER BY start_date
-LIMIT 10;
+ORDER BY reservations.start_date
+LIMIT 10
